7bd7cc96f88d744fdf000db47fb6df35d1a562ec
jlt717 <128320420+jlt717@users.noreply.github.com>
2023-06-03 11:39:21 -0500

    Updated code

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -11,21 +11,22 @@ export const MainView = () => {
     fetch("https://cinedex.herokuapp.com/movies")
       .then((response) => response.json())
       .then((data) => {
-        console.log(data);
-        const moviesFromApi = data.map((movie) => {
-          return {
-            _id: movie.id,
-            ImagePath: movie.ImagePath,
-            Title: movie.Title,
-            Released: movie.Released,
-            Description: movie.Description,
-            Genre: movie.Genre,
-            Director: movie.Director,
-            Featured: movie.Featured.toString(),
-          };
-        });
+        setMovies(data);
+        //console.log(data);
+        // const moviesFromApi = data.map((movie) => {
+        //   return {
+        //     _id: movie._id,
+        //     ImagePath: movie.ImagePath,
+        //     Title: movie.Title,
+        //     Released: movie.Released,
+        //     Description: movie.Description,
+        //     Genre: movie.Genre,
+        //     Director: movie.Director,
+        //     Featured: movie.Featured.toString(),
+        //   };
+        // });
 
-        setMovies(moviesFromApi);
+        //setMovies(moviesFromApi);
       });
   }, []);
 
