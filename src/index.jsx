959d288c3ca0ff5491c72ce769a4374408bde27b
jlt717 <128320420+jlt717@users.noreply.github.com>
2023-06-10 12:12:24 -0500

    Removed unnecessary code

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,9 @@
 // Import statement to indicate that you need to bundle `./index.scss`
 
-import React from "react";
 import { createRoot } from "react-dom/client";
 import { MainView } from "./components/main-view/main-view";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Container } from "react-bootstrap";
-import "./index.scss";
 
 // Main component (will eventually use all the others)
 const CinedexApplication = () => {
