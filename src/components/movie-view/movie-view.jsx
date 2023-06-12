26ad27751ad5c4ea5cab145184d1674839c86316
jlt717 <128320420+jlt717@users.noreply.github.com>
2023-06-12 11:22:05 -0500

    changed b to m for consistency

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -7,7 +7,7 @@ import "./movie-view.scss";
 export const MovieView = ({ movies }) => {
   const { movieId } = useParams();
 
-  const movie = movies.find((b) => b.id === movieId);
+  const movie = movies.find((m) => m.id === movieId);
 
   return (
     <div>
