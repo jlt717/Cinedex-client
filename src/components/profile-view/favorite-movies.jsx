59a70ca5c86ffbca12249633e3cb86f0e94ed3dc
jlt717 <128320420+jlt717@users.noreply.github.com>
2023-06-14 07:10:06 -0500

    Added cols and rows

--- src/components/profile-view/favorite-movies.jsx
+++ src/components/profile-view/favorite-movies.jsx
@@ -1,25 +1,28 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import Button from "react-bootstrap";
+import { Row, Button, Col } from "react-bootstrap";
 
-export const FavoriteMovies = ({ favoriteMovies, onRemoveFavorite }) => {
+export const FavoriteMovies = ({ favoriteMovies, deleteFromFavorites }) => {
+  console.log(favoriteMovies);
   return (
-    <div>
+    <Row>
       <h2>Favorite Movies</h2>
       {favoriteMovies.map((movie) => (
-        <div key={movie._id}>
-          <img src={movie.ImageUrl} />
+        <Col md={2} key={movie._id}>
+          <div>
+            <img src={movie.ImageURL} className="img-fluid" />
+          </div>
           <Link to={`/movies/${movie.Title}`}>
             <h4>{movie.Title}</h4>
           </Link>
           <button
             variant="secondary"
-            onClick={() => onRemoveFavorite(movies._id)}
+            onClick={() => deleteFromFavorites(movie._id)}
           >
             Remove from favorites
           </button>
-        </div>
+        </Col>
       ))}
-    </div>
+    </Row>
   );
 };
