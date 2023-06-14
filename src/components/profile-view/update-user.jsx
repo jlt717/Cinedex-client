2cf5441b40d553aa80a0387606aeedbc70300bfa
jlt717 <128320420+jlt717@users.noreply.github.com>
2023-06-14 07:11:03 -0500

    updated parameters

--- src/components/profile-view/update-user.jsx
+++ src/components/profile-view/update-user.jsx
@@ -1,62 +1,66 @@
 import React from "react";
-import { Form, Button, Card, CardGroup, Container } from "react-bootstrap";
+import { useState } from "react";
+import {
+  Form,
+  Button,
+  Card,
+  CardGroup,
+  Container,
+  Row,
+  Col,
+} from "react-bootstrap";
 
-export const UpdateUser = ({ handleSubmit, handleUpdate }) => {
+export const UpdateUser = ({ handleSubmit, onHandleChange, user }) => {
+  const [data, setData] = useState(user);
   //function updateUser({})
-  const handleUpdate = (event) => {
-    const updatedValue = event.target.value;
-    setInputValue(updatedValue);
-
-    return (
-      <Container>
-        <Row>
-          <Col>
-            <CardGroup>
-              <Card>
-                <Form
-                  className="profile-form"
-                  onSubmit={(e) => handleSubmit(e)}
-                >
-                  <h2> Want to change your info?</h2>
-                  <label>Username:</label>
-                  <input
-                    type="text"
-                    name="Username"
-                    defaultValue={user.Username}
-                    onChange={(e) => handleUpdate(e)}
-                  />
-
-                  <label>Password</label>
-                  <input
-                    type="password"
-                    name="password"
-                    defaultValue={user.Password}
-                    onChange={(e) => handleUpdate(e)}
-                  />
-
-                  <label>Email address</label>
-                  <input
-                    type="email"
-                    name="email"
-                    defaultValue={user.Email}
-                    onChange={(e) => handleUpdate(e.target.value)}
-                  />
-                  <label>Birthdate</label>
-                  <input
-                    type="birthday"
-                    name="birthday"
-                    defaultValue={user.Birthday}
-                    onChange={(e) => handleUpdate(e.target.value)}
-                  />
-                  <Button variant="primary" type="submit">
-                    Edit
-                  </Button>
-                </Form>
-              </Card>
-            </CardGroup>
-          </Col>
-        </Row>
-      </Container>
-    );
+  const onChange = (event) => {
+    onHandleChange((prevState) => ({
+      ...prevState,
+      [event.target.name]: event.target.value,
+    }));
   };
+
+  return (
+    <Container>
+      <Row>
+        <Col>
+          <CardGroup>
+            <Card>
+              <Form className="profile-form" onSubmit={(e) => handleSubmit(e)}>
+                <h2> Want to change your info?</h2>
+                <label>Username:</label>
+                <input
+                  type="text"
+                  name="Username"
+                  defaultValue={user.Username}
+                  onChange={onChange}
+                />
+
+                <label>Password</label>
+                <input type="password" name="password" onChange={onChange} />
+
+                <label>Email address</label>
+                <input
+                  type="email"
+                  name="email"
+                  value={user.Email}
+                  onChange={onChange}
+                />
+                <label>Birthdate</label>
+                <input
+                  type="date"
+                  name="birthday"
+                  value={user.Birthday}
+                  onChange={onChange}
+                />
+                <Button variant="primary" type="submit">
+                  Edit
+                </Button>
+              </Form>
+            </Card>
+          </CardGroup>
+        </Col>
+      </Row>
+    </Container>
+  );
 };
